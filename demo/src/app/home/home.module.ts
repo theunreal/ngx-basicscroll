1856e98f560d6f4b6bb3f8bac471639b3a7da594
Eliran Elnasi (eelnasi) <eelnasi@cisco.com>
2018-03-03 18:48:16 +0200

    Demo Fix

--- demo/src/app/home/home.module.ts
+++ demo/src/app/home/home.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import {BasicScrollModule} from '../../../../src';
+import {BasicScrollModule} from 'ngx-basicscroll';
 
 @NgModule({
     imports: [
--- src/module/component/basicscroll.component.ts
+++ src/module/component/basicscroll.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, Input, OnInit, Optional, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 declare const require;
 const basicScroll = require('basicScroll');
 
