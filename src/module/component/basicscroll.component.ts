174951cb3dd902bd4d160e67c213e077df4bcb9b
Eliran Elnasi (eelnasi) <eelnasi@cisco.com>
2018-03-04 08:18:26 +0200

    Stronger type check, add FromPosition and toPosition properties

--- src/module/component/basicscroll.component.ts
+++ src/module/component/basicscroll.component.ts
@@ -11,9 +11,11 @@ const basicScroll = require('basicScroll');
 export class BasicScrollComponent implements AfterViewInit, OnInit {
 
   @ViewChild('element') element: ElementRef;
-  @Input('from') from = 'top-middle';
-  @Input('to') to = 'top-top';
-  @Input('type') type: string;
+  @Input('fromPosition') fromPosition = 'top-middle';
+  @Input('toPosition') toPosition = 'top-top';
+  @Input('fromValue') fromValue = '0';
+  @Input('toValue') toValue = '100px';
+  @Input('type') type: 'rotate' | 'fade' | 'translateX' | 'translateY';
   @Input('timing') timing = 'expoIn';
 
   bsOptions: any;
@@ -22,8 +24,8 @@ export class BasicScrollComponent implements AfterViewInit, OnInit {
   ngOnInit() {
     this.bsOptions = {
       elem: this.element.nativeElement,
-      from: this.from,
-      to: this.to,
+      from: this.fromPosition,
+      to: this.toPosition,
       timing: this.timing
     };
 
@@ -32,34 +34,34 @@ export class BasicScrollComponent implements AfterViewInit, OnInit {
         this.useClass = 'rotateBox';
         this.updateProps(  {
           '--r': {
-            from: '0',
-            to: '1turn'
+            from: this.fromValue,
+            to: this.toValue
           }});
         break;
       case 'fade':
         this.useClass = 'fadeBox';
         this.updateProps(  {
           '--o': {
-            from: '.01',
-            to: '.99'
+            from: this.fromValue,
+            to: this.toValue
           }});
         break;
-      case 'easeX':
+      case 'translateX':
         this.bsOptions.direct = true;
         this.useClass = 'easeX';
         this.updateProps(  {
           '--tx': {
-            from: '0',
-            to: '100px'
+            from: this.fromValue,
+            to: this.toValue
           }});
         break;
-      case 'easeY':
+      case 'translateY':
         this.bsOptions.direct = true;
         this.useClass = 'easeY';
         this.updateProps(  {
           '--ty': {
-            from: '0',
-            to: '100px'
+            from: this.fromValue,
+            to: this.toValue
           }});
         break;
     }
